279010d2ff2b89141f6d28d0cef4075248fb118d
ShubhamBhardwaj <shubham.11901502@lpu.in>
2021-03-05 16:57:19 +0530

    1st question

--- Arrays/arrayRotation.js
+++ Arrays/arrayRotation.js
@@ -1,6 +1,26 @@
-// ❤ lovely way to do this :)
+// Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
 
-const arrayRotation = (arr, times) =>
-  arr.concat(arr).slice(times, times + arr.length);
+const rotate = (arr, last, aux) => {
+  //this function will swap the element on last index with i
+  let temp = arr[last];
+  arr[last] = arr[aux];
+  arr[aux] = temp;
+};
 
-console.log(arrayRotation([1, 2, 3, 4, 5, 6, 7], 2));
+const arrayRotation = (arr, d, n) => {
+  if (d < 0) {
+    return arr;
+  }
+  d = d % n; //Eliminating Extra Loops
+  if (d === 0) {
+    return arr; //if d is 0 or in multiples of n no need of swaps
+  }
+  for (let j = 0; j < d; j++) {
+    for (let i = n - 2; i >= 0; i--) {
+      rotate(arr, n - 1, i);
+    }
+  }
+  return arr;
+};
+
+console.log(arrayRotation([1, 2, 3, 4, 5], 1, 5));
