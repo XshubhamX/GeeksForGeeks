423b9f3bf580bece0b1842940e7f9dfea3d15a8d
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-03-05 15:56:22 +0530

    array Rotation question

--- Arrays/arrayRotation.js
+++ Arrays/arrayRotation.js
@@ -1,3 +1,15 @@
-const arrayRotation = () => {};
+// brute force method time complexity *** O(arr.length * times) ***
 
-arrayRotation();
+// const arrayRotation = (arr, times) => {
+//   for (let i = 0; i < times; i++) {
+//     arr.unshift(arr.pop());
+//   }
+//   return arr;
+// };
+
+// ❤ lovely way to do this :)
+
+const arrayRotation = (arr, times) =>
+  arr.concat(arr).slice(times, times + arr.length);
+
+console.log(arrayRotation([1, 2, 3, 4, 5, 6, 7], 2));
