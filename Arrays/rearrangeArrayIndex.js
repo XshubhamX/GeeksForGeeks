1bd6ba3e2b418a60a87fc2406882f1f76a8a5bbd
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-03-08 13:30:32 +0530

    rearrange array such that a[i]=i

--- Arrays/rearrangeArrayIndex.js
+++ Arrays/rearrangeArrayIndex.js
@@ -19,16 +19,21 @@
 // };
 
 const rearrange = (arr) => {
-  let temp;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= 0 && arr[i] !== i) {
-      // swapping
-      [arr[arr[i]], arr[i]] = [arr[i], arr[arr[i]]];
+  let obj = {};
+  let i = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      obj[arr[i]] = arr[i];
+    }
+  }
+  for (i = 0; i < arr.length; i++) {
+    if (obj[i] || obj[i] === 0) {
+      arr[i] = obj[i];
     } else {
-      i++;
+      arr[i] = -1;
     }
   }
   return arr;
 };
 
-console.log(rearrange([-1, 3, -1, 2, 5, 6]));
+console.log(rearrange([-1, 3, -1, 2, 5]));
