40146dccc226defba40378a8b2e5846f3ab673f9
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-03-08 09:40:11 +0530

    logic rearrange

--- Arrays/rearrangeArrayIndex.js
+++ Arrays/rearrangeArrayIndex.js
@@ -1,20 +1,32 @@
 // Rearrange an array such that arr[i] = i
 
 // brute force tech*   O(n^2)
+// const rearrange = (arr) => {
+//   let temp;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] === i) {
+//         // swapping logic
+//         temp = arr[j];
+//         arr[j] = arr[i];
+//         arr[i] = temp;
+//         break;
+//       }
+//     }
+//   }
+//   return arr;
+// };
+
 const rearrange = (arr) => {
   let temp;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === i) {
-        // swapping logic
-        temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-        break;
-      }
+    if (arr[i] >= 0 && arr[i] !== i) {
+      // swapping
+      [arr[arr[i]], arr[i]] = [arr[i], arr[arr[i]]];
+    } else {
+      i++;
     }
   }
   return arr;
 };
-
 console.log(rearrange([-1, -1, 6, 1, 9, 3, 2, -1, 4, -1]));
