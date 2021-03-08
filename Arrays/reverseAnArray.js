f1646812cbe711d590d120d31ba465c3b4a98159
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-03-08 09:52:55 +0530

    reverse an array

---
+++ Arrays/reverseAnArray.js
@@ -0,0 +1,16 @@
+// Given an array (or string), the task is to reverse the array/string.
+
+// iteriative way to do this
+const reverse = (arr) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+  return arr;
+};
+// using inbuilt function
+const reverse = (arr) => arr.reverse();
+console.log(reverse([1, 2, 3]));
