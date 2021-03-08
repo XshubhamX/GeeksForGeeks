6ae823a0140e28bcc6413e4513154b1d22f70d65
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-03-08 16:29:49 +0530

    move zeros using single traversal

--- Arrays/reArrangeZeros.js
+++ Arrays/reArrangeZeros.js
@@ -1,17 +1,29 @@
 // Move all zeroes to end of array
 
+// const arrange = (arr) => {
+//   let count = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       arr[count] = arr[i];
+//       count++;
+//     }
+//   }
+//   while (count < arr.length) {
+//     arr[count++] = 0;
+//   }
+//   return arr;
+// };
+
 const arrange = (arr) => {
-  let count = 0;
+  let j = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== 0) {
-      arr[count] = arr[i];
-      count++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j++;
     }
-  }
-  while (count < arr.length) {
-    arr[count++] = 0;
   }
   return arr;
 };
+//TODO // Move all zeroes to end of array nOW DO IT USING SINGLE TRAVERSAL
 
-console.log(arrange([1, 2, 0, 4, 50, 0]));
+console.log(arrange([0, 1, 2, 0, 4, 50, 0, 0, -12]));
