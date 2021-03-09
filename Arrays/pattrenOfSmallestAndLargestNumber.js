04e80a7fa46c32767efc7fe5ddc02a9f52b2e99a
jasbirrajrana <jasbirrajrana6699@gmail.com>
2021-03-09 18:13:41 +0530

    small ..large 2nd small...2nd large

---
+++ Arrays/pattrenOfSmallestAndLargestNumber.js
@@ -0,0 +1,23 @@
+// Given an array of integers, task is to print the array in the order – smallest number, Largest number, 2nd smallest number, 2nd largest number, 3rd smallest number, 3rd largest number and so on….
+
+const solver = (arr) => {
+  let result = [];
+  arr = arr.sort();
+
+  let l = 0;
+  let r = arr.length - 1;
+  while (l <= r) {
+    if (l === r) {
+      result.push(arr[l]);
+      l++;
+    } else {
+      result.push(arr[l]);
+      result.push(arr[r]);
+      l++;
+      r--;
+    }
+  }
+  return result;
+};
+
+console.log(solver([5]));
